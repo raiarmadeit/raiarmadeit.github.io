7ddbb60dba2a793e36f35752fda37a5fb0364fd9
cloudcodes434 <copmi175@gmail.com>
2024-12-28 00:15:15 +0000

    Huge Update

--- Core.js
+++ Core.js
@@ -1,24 +1,77 @@
 window.addEventListener('scroll', function() {
-    const aboutOption = document.querySelector('.AboutOption');
-    const scriptOption = document.querySelector('.ScriptOption');
     const Title = document.querySelector('.Title'); 
+    const About53 = document.querySelector('.About53');
+    const VideoButton = document.querySelector('.VideoButton');
+    const CoreContacts = document.querySelector('.CoreContact')
     const scrollPosition = window.scrollY;
 
     let opacity = 1 - scrollPosition / 200; 
     opacity = Math.max(opacity, 0);
 
-    aboutOption.style.opacity = opacity;
-    scriptOption.style.opacity = opacity;
+    VideoButton.style.opacity = opacity;
+    About53.style.opacity = opacity;
     Title.style.opacity = opacity;
+    CoreContacts.style.opacity = opacity;
+});
+
+
+const video1 = document.querySelector('.Video1');
+const videoObserver = new IntersectionObserver((entries) => {
+    entries.forEach(entry => {
+        if (entry.isIntersecting) {
+            entry.target.style.opacity = 1;
+        } else {
+            entry.target.style.opacity = 0; 
+        }
+    });
+}, { threshold: 0.5 });
+
+videoObserver.observe(video1);
+
+const video2 = document.querySelector('.Video2');
+const video2Observer = new IntersectionObserver((entries) => {
+    entries.forEach(entry => {
+        if (entry.isIntersecting) {
+            entry.target.style.opacity = 1;
+        } else {
+            entry.target.style.opacity = 0;
+        }
+    });
+}, { threshold: 0.5 });
+
+
+video2Observer.observe(video2);
+
+
+const video3 = document.querySelector('.Video3');
+const video3Observer = new IntersectionObserver((entries) => {
+    entries.forEach(entry => {
+        if (entry.isIntersecting) {
+            entry.target.style.opacity = 1;
+        } else {
+            entry.target.style.opacity = 0; 
+        }
+    });
+}, { threshold: 0.5 });
+
+video3Observer.observe(video3);
+
+
+
+document.addEventListener('DOMContentLoaded', function() {
+    const Video23 = document.getElementById('Video23');
+    const VideoScole = document.getElementById('VideoSection2');
+
+    Video23.addEventListener('click', function() {
+        VideoScole.scrollIntoView({ behavior: 'smooth' });
+    });
 });
 
 document.addEventListener('DOMContentLoaded', function() {
     const scrollButton = document.getElementById('scrollButton');
-    const scriptImage = document.getElementById('scriptImage');
+    const AboutRaiarID = document.getElementById('AboutRaiarID');
 
     scrollButton.addEventListener('click', function() {
-        scriptImage.scrollIntoView({
-            behavior: 'smooth'
-        });
+        AboutRaiarID.scrollIntoView({ behavior: 'smooth' });
     });
 });
